b55a8d01b1827b6b154718b2e7ee8dbc6dbb271f
william UNC <wmiranda696@gmail.com>
2024-03-08 12:19:45 -0800

    Tp-1.2

--- practica/tp-complejidad/code/ejercicio5.py
+++ practica/tp-complejidad/code/ejercicio5.py
@@ -11,13 +11,13 @@ def contiene_suma(A,n):
 	    if complemento  in visitos:
 	    
 	        return True
-
-        #Si no no encntramos ningun para que sume n, retornamos FALSO
-        vistos.add(num)
+		#Si no no encntramos ningun para que sume n, retornamos FALSO
+		visitos.add(num)
     return False
 
 A = [1,2,3,4,5]
 n = 9
+print("Lista: ",A)
 print("¿La lista A contiene un par de elementos que suman",n,"? ", contiene_suma(A,n) )
 #la compejiad del algoritmo seria O(n) ya que solo recoremos la lista una vez y realiza las operaciones en tiempo constante en cada iteracion.
 
