6e1a11ca30fb23e6341d379efd54b669db86c298
WilHelm-P11 <wmiranda696@gmail.com>
2024-03-08 22:34:29 -0300

    Tp-1.4

--- practica/tp-complejidad/code/ejercicio5.py
+++ practica/tp-complejidad/code/ejercicio5.py
@@ -1,22 +1,21 @@
 def contiene_suma(A,n):
-	#Creamos un conjunto para almacenar los elementos vistos
-	vistos=set()
-	
-	#Iteramos sobre la lista A, con el for visitamos cada elemento de la lista
-	for num in A:
+    #Creamos un conjunto para almacenar los elementos vistos
+    vistos=set()
 
-        #Calculamos el complemeento nesesrio para que la suma sea n
-	    complemento = n - num
-        #Verifficamos si el complemento ya esta en el conjunto de elementos vistos  
-	    if complemento  in visitos:
-	    
-	        return True
-		#Si no no encntramos ningun para que sume n, retornamos FALSO
-		visitos.add(num)
+    #iteramos sobre la lista A, y visitamos cada elemento de la lista
+    for num in A:
+        #calculamos el complemento esesario para que la suma sea n
+        complemento = n - num
+        #Verificamos si el complemento ya esta en la lista de elementos visitados
+        if complemento in vistos:
+			#Si seencuentra un par que dado su suma da n
+            return True
+        vistos.add(num)
+	#Si no se a ancontrado entonces retorna falso
     return False
 
-A = [1,2,3,4,5]
-n = 9
+A=[1,2,3,4,5]
+n=9
 print("Lista: ",A)
 print("¿La lista A contiene un par de elementos que suman",n,"? ", contiene_suma(A,n) )
 #la compejiad del algoritmo seria O(n) ya que solo recoremos la lista una vez y realiza las operaciones en tiempo constante en cada iteracion.
